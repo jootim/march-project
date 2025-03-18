92b664c37070aebfa660031760f3a2e09fc599cf
werayutjoot <jootim@gmail.com>
2025-03-18 19:02:39 +0700

    add accounts app

--- core/core/urls.py
+++ core/core/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('members.urls')),  # Ensure you're importing the 'urls' module from the 'members' app
+    path('accounts', include('accounts.urls')),  # Ensure you're importing the 'urls' module from the 'members' app
 ]
